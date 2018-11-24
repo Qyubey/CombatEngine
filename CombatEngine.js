8484ccbe683df79aaf904de91ebfd3b0b53f4345
Qyubey <bigwoodencastle@gmail.com>
2018-11-24 21:34:50 +1000

    General bugfixing. Checking target acquisition is valid.

--- CombatEngine.js
+++ CombatEngine.js
@@ -56,6 +56,8 @@ function Unit (name, unitName, wSystems, hp, sp, evasion) {
     this.wSystems = wSystems;
     this.hp = hp;
     this.sp = sp;
+    this.hpMax = hp;
+    this.spMax = sp;
     this.evasion = evasion;
     this.kills = 0;
     this.totalDamageDealt = 0;
@@ -91,36 +93,28 @@ const printArray = function (logArray) {
 }
 
 const constructString = function (log) {
-    // Record amount of hits, amount of misses.
-    // Record total damage to sp and hp
-    // Record any kills
-    let resultString = "";
-    if (log.hits > 0) {
-        resultString += log.atk.name + " landed " + log.hits + " hits this turn.";
-        resultString += "\n";
-    }
-    // if (log.misses > 0) {
-    //     resultString += log.atk.name + " missed " + log.def.name + " " + log.misses + " times with a " + log.atk.weapons[0].name + ".";
-    //     resultString += "\n";
-    // }
-    if (log.spDam > 0) {
-        resultString += log.atk.name + " dealt " + log.spDam + " shield damage.";
-        resultString += "\n";
-    }
-    if (log.hpDam > 0) {
-        resultString += log.atk.name + " dealt " + log.hpDam + " hull damage.";
-        resultString += "\n";
-    }
-    if (log.sBreaks > 0) {
-        resultString += log.def.name + " has lost shields.";
-        resultString += "\n";
-    }
-    if (log.kills > 0) {
-        resultString += log.def.name + "Ship has been destroyed.";
-        resultString += "\n";
-    }
-    printLog(resultString);
-    return resultString;
+    let logString = "";
+
+    if (!log.def) return "DEBUG: All defenders killed. Invalid target.";
+
+    // How many hits
+    if (log.hits > 0) logString += log.atk.name + " hit " + log.def.name + " " + log.hits + " times using " + log.system.name + ". ";
+    else logString += log.atk.name + " missed their shots against " + log.def.name + ". ";
+    // Tabulate all damage against a single target's shields.
+    if (log.spDam > 0) logString += log.atk.name + " dealt " + log.spDam + " shield damage. ";
+    // If they lost shields.
+    if (log.sBreak > 0) logString += " " + log.def.name + " has lost shields! ";
+    // Tabulate all damage against a single target's hull.
+    if (log.hpDam > 0) logString += log.atk.name + " dealt " + log.hpDam + " hull damage. ";
+    // If they were disabled.
+    if (log.disables > 0) logString += " " + log.def.name + " has been disabled! ";
+    // If they were destroyed.
+    if (log.kills > 0) logString += " " + log.def.name + " has been destroyed! ";
+    else logString += log.def.name + ": ";
+    if (log.def.sp > 0) logString += log.def.sp + "/" + log.def.spMax + " SP. ";
+    if (log.def.hp > 0)  logString += log.def.hp + "/" + log.def.hpMax + " HP.";
+
+    return logString;
 };
 
 
@@ -136,7 +130,7 @@ const percentileRoll = function () {
  * Returns a random number for damage calculation.
  */
 const damageRoll = function (max) {
-    return Math.floor(Math.random()*(max-1)) + 1;
+    return Math.floor(Math.random()*max) + 1;
 };
 
 /**
@@ -144,11 +138,11 @@ const damageRoll = function (max) {
  */
 const removeUnit = function (unit, array, deadArray) {
     const index = array.indexOf(unit);
-    if (index !== -1) {
-        array.splice(index, 1);
-    }
     if (!!deadArray) {
         deadArray.push(unit);
+    }
+    if (index !== -1) {
+        array.splice(index, 1);
     }
 }
 
@@ -165,128 +159,136 @@ const selectTarget = function (array) {
 /**
  * Calculates damage for an attack. Assigns to either shields or hull, and reduces by armour.
  */
-const damageCalc = function (logData, weapon, atk, def) {
-    let logString = "";
+const damageCalc = function (logObjTurn, weapon, atk, def) {
     let damage = damageRoll(weapon.damage);
+
     if (def.sp === 0) {
         def.hp -= damage;
 
-        // Add Log Data
-        logString += atk.name + " dealt " + damage + " hull damage to " + def.name + " using " + weapon.name + ".";
-
         if (def.hp <= 0) {
             def.hp = 0;
-            def.state = 'Destroyed';
+            def.state = 'destroyed';
 
             // Add Log Data
-            logString += " " + def.name + " has been destroyed!";
             atk.kills += 1;
-            logData.kills += 1;
-        } else {
-            logString += " " + def.name + ": " + def.hp + "/10 Hull Points.";
+            logObjTurn.kills += 1;
         }
 
         // Add Log Data
         atk.totalDamageDealt += damage;
-        logData.hpDam += damage;
+        logObjTurn.hpDam += damage;
 
     } else {
         def.sp -= damage;
-
-        // Add Log Data
-        logString += atk.name + " dealt " + damage + " shield damage to " + def.name + " using " + weapon.name + ".";
 
         if (def.sp <= 0) {
             def.sp = 0;
 
             // Add Log Data
-            logString += " " + def.name + " has lost shields!";
-            logData.sBreak += 1;
-        } else {
-            logString += " " + def.name + ": " + def.sp + "/10 Shield Points.";
+            logObjTurn.sBreak += 1;
         }
 
         // Add Log Data
         atk.totalDamageDealt += damage;
-        logData.spDam += damage;
+        logObjTurn.spDam += damage;
     }
-    logData.push(logString);
 };
 
 /**
  * Behaviour for General Unit attack.
  */
-const behaviourAttack = function (logData, atk, targetsArray) {
+const behaviourAttack = function (logArray, atk, targetSection) {
+    let logObjTurn = {};
+    logObjTurn.hits = 0;
+    logObjTurn.atk = atk;
+    logObjTurn.hpDam = 0;
+    logObjTurn.spDam = 0;
+    logObjTurn.kills = 0;
+    logObjTurn.sBreak = 0;
 
     atk.wSystems.forEach(function(system) {
-        let targetedSection = selectTarget(targetsArray);
-        if (targetedSection.units.length !== 0) {
-            let def = selectTarget(targetedSection.units);
-            let evasion = def.evasion;
-            
-            // For each weapon, roll an attack and resolve the damage
-            // If we destroy the unit, remove it from targets and into casualties.
-            // Check that our target is destroyed before we calc attack.
-            system.weapons.forEach(function(weapon) {
-                // If the unit has not been destroyed.
-                if (def.state !== "Destroyed") {
-                    const roll = percentileRoll();
+        logObjTurn.system = system;
+        let def = selectTarget(targetSection.units);
+        let evasion = def.evasion;
+        logObjTurn.def = def;
 
-                    // If attack hits, calculate damage.
-                    if (roll >= evasion) {
+        // For each weapon, roll an attack and resolve the damage
+        // If we destroy the unit, remove it from targets and into casualties.
+        // Check that our target is destroyed before we calc attack.
+        system.weapons.forEach(function(weapon) {
+            // If the unit has not been destroyed.
+            if (def.state === "active") {
+                const roll = percentileRoll();
 
-                        // Log Data
-                        logData.hits += 1;
+                // If attack hits, calculate damage.
+                if (roll >= evasion) {
 
-                        damageCalc(logData, weapon, atk, def);
+                    // Log Data
+                    logObjTurn.hits += 1;
 
-                        if (def.state === "Destroyed") {
-                            removeUnit(def, targetedSection.units, targetedSection.casualties);
-                        }
-                    } else {
-                        logData.push(atk.name + " missed a shot.");
+                    damageCalc(logObjTurn, weapon, atk, def);
+
+                    if (def.state === "destroyed") {
+                        removeUnit(def, targetSection.units, targetSection.casualties);
                     }
                 }
-            })
-        }
+            } else {
+                console.log(atk.name + " has destroyed this unit. Shot misses.");
+            }
+        })
     })
+
+    // Log result of behaviour
+    logArray.push(constructString(logObjTurn));
 };
 
 /**
  * Determines if it is the turn for any of the sectionsArray given.
  */
 const passTurn = function (sectionsArray) {
-    // Generate a Log Object
-    let logData = [];
+    // Generate a Log Array
+    let logArray = [];
 
-    // For each section, determine if it is the section's turn.
-    sectionsArray.forEach(function(section) {
-        if (section.speed === 0) {
-            logData.push(section.name + " takes a turn. " + section.units.length + " units available.");
-            logData.push("----------");
+    // For each section, check if it is the section's turn.
+    sectionsArray.forEach(function(activeSection) {
+        if (activeSection.speed === 0) {
+            logArray.push("----------");
+            logArray.push(activeSection.name + " takes a turn. " + activeSection.units.length + " units available.");
+            logArray.push("----------");
 
-            // Create possible targets array.
+            // Filter this section out of the list of sections.
+            // TODO: Filter out friendly sections too.
             let targetSectionsArray = sectionsArray.filter(function(target) {
-                return target.name !== section.name;
+                return target.name !== activeSection.name && target.units.length > 0;
             });
 
+            // Select a random section. All units of active section will attack the units within.
+            let targetedSection = selectTarget(targetSectionsArray);
+
             // TODO: Determine Behaviour of Section here.
-            // TODO: Alter behaviour based on Morale here.
-            // TODO: For each unit in section, execute behaviour.
-            section.units.forEach(function(unit) {
-                logData.push(unit.name + " flies in!");
-                behaviourAttack(logData, unit, targetSectionsArray);
-                logData.push("-----");
+
+            activeSection.units.forEach(function(unit) {
+
+                // TODO: Alter behaviour based on Morale here.
+                // TODO: For each unit in section, execute behaviour.
+
+                // Check that there are any possible units left to target in the section.
+                if (targetedSection.units.length > 0) {
+                    behaviourAttack(logArray, unit, targetedSection);
+                } else {
+                    console.log(unit.name + " cannot find any units left in the enemy section.")
+                }
             });
 
             // Reset speed/turn timer.
-            section.speed = Math.floor(Math.random()*3);
+            activeSection.speed = Math.floor(Math.random()*3);
         } else { // If not your turn, tick speed.
-            section.speed -= 1;
+            activeSection.speed -= 1;
         }
     });
 
-    printArray(logData);
+    // Print log array.
+    printArray(logArray);
 };
 
 
